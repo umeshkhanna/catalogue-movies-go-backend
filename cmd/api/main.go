60d0135a774ce2d80158d74c71f5994311348866
UmeshkhannaM <umeshkhannan@gmail.com>
2022-12-30 23:11:34 +0530

    db connection added

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,11 +10,17 @@ import (
 const port = 8080
 
 type application struct {
+	DSN    string
 	Domain string
 }
 
 func main() {
 	var app application
+	flag.StringVar(&app.DSN,
+		"dsn",
+		"host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5",
+		"Postgres connection string")
+	flag.Parse()
 
 	app.Domain = "example.com"
 	log.Println("Starting application on port", port)
